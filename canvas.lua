f03d0d1360758af80117a78136897f93b88d7487
andrea <marcosano.andrea@gmail.com>
2023-08-21 17:49:25 +0800

    before branching

--- canvas.lua
+++ canvas.lua
@@ -6,10 +6,11 @@ local fillColor = { default={1,0,0,1}, over={1,0,4,1} }
 local stroke = { default={0,0,0,1},over={0,0,0,1}}
 
 local function createCanvas(parent,x,y)
-    canvas=display.newRect(parent,x,y,100,100); 
-    canvas:setFillColor(0,0,0)
+    canvas=display.newRoundedRect(parent,x,y,width-width/7,width-width/7,50); 
+    canvas:setFillColor(255,255,255)
     canvas:setStrokeColor(0,0,0)
-    
+    canvas.strokeWidth=8
+
     return canvas
 end
 function scene:create(event)
@@ -20,8 +21,13 @@ function scene:create(event)
     rect:setFillColor(255,255,255)
     sceneGroup:insert(rect)
 
-    local canvas=createCanvas(sceneGroup,width/2,height/3)
+    local canvas=createCanvas(sceneGroup,width/2,height/2.7)
     sceneGroup:insert(canvas)
+
+    local star = display.newLine( 200, 90, 0,0 )
+   
+    star:setStrokeColor( 1, 0, 0, 1 )
+    star.strokeWidth = 8
 
     
 
